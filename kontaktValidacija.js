229fb80cf65a0778b71c468fd95f84bc6bbfabf3
Dina <dinaahmic@hotmail.com>
2015-04-15 22:31:49 +0200

    validacija kontakt forme update, dodano stablo
    dodala graficki element za prikaz kod validacije forme; promijenila
    regex za validaciju email-a; dodala stablo (na fakultet_studij.html)

--- kontaktValidacija.js
+++ kontaktValidacija.js
@@ -9,42 +9,54 @@ function kontaktValidacija(){
 	var por = false;
 	
 	var imePrezime = /^([A-ZŠĐČĆŽ][a-zšđčćž][a-zšđčćž]([a-zšđčćž]+)?[\s][A-ZŠĐČĆŽ][a-zšđčćž][a-zšđčćž]([a-zšđčćž]+)?)+$/;
-	var mailRegex = /^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$/;	
+	var mailRegex = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;	
 	
 	if(!imePrezime.test(ime.value)){
 		ime.style.borderColor = "red";
 		validna = false;
 		imep = true;
+		//document.getElementByClassName('validacija_slika').style.display="inline-block";
+		document.getElementById('slika1').style.display="inline-block";
 		document.getElementById('obavezno').style.display="inline";
 	}
 	else{
 		ime.style.borderColor = "";
 		imep = false;
 		document.getElementById('obavezno').style.display="none";
+		document.getElementById('slika1').style.display="none";
+		//document.getElementByClassName('validacija_slika').style.display="none";
 	}
 	
 	if(!mailRegex.test(mail.value)){
 		mail.style.borderColor = "red";
 		validna = false;
 		maill = true;
+		//document.getElementByClassName('validacija_slika').style.display="inline-block";
+		document.getElementById('slika2').style.display="inline-block";
 		document.getElementById('obaveznoo').style.display="inline";
 	}
 	else{
 		mail.style.borderColor = "";
 		maill = false;
 		document.getElementById('obaveznoo').style.display="none";
+		document.getElementById('slika2').style.display="none";
+		//document.getElementByClassName('validacija_slika').style.display="none";
 	}
 	
 	if(poruka.value == null || poruka.value == '' || poruka.value.length < 5){
 		poruka.style.borderColor = "red";
 		validna = false;
 		por = true;
+		//document.getElementByClassName('validacija_slika').style.display="inline-block";
+		document.getElementById('slika3').style.display="inline-block";
 		document.getElementById('poruka').style.display="inline";
 	}
 	else{
 		poruka.style.borderColor = "";
 		por = false;
 		document.getElementById('poruka').style.display="none";
+		document.getElementById('slika3').style.display="none";
+		//document.getElementByClassName('validacija_slika').style.display="none";
 	}
 	
 	if(imep){
---
+++ stablo.js
@@ -0,0 +1,83 @@
+function bachelor(){
+	var x=document.getElementById('bnivo1plus');
+	if (x.innerHTML=="+"){
+		x.innerHTML="-";
+		document.getElementById('bnivo2').style.display="block";
+	}
+	else {
+		x.innerHTML="+";
+		document.getElementById('bnivo2').style.display="none";
+	}
+}
+
+function bachelor1(){
+	var x=document.getElementById('b1plus');
+	if (x.innerHTML=="+"){
+		x.innerHTML="-";
+		document.getElementById('bpnivo3').style.display="block";
+	}
+	else {
+		x.innerHTML="+";
+		document.getElementById('bpnivo3').style.display="none";
+	}
+}
+
+function bachelor2(){
+	var x=document.getElementById('b2plus');
+	if (x.innerHTML=="+"){
+		x.innerHTML="-";
+		document.getElementById('bdnivo3').style.display="block";
+	}
+	else {
+		x.innerHTML="+";
+		document.getElementById('bdnivo3').style.display="none";
+	}
+}
+
+function bachelor3(){
+	var x=document.getElementById('b3plus');
+	if (x.innerHTML=="+"){
+		x.innerHTML="-";
+		document.getElementById('btnivo3').style.display="block";
+	}
+	else {
+		x.innerHTML="+";
+		document.getElementById('btnivo3').style.display="none";
+	}
+}
+
+function master(){
+	var x=document.getElementById('mnivo1plus');
+	if (x.innerHTML=="+"){
+		x.innerHTML="-";
+		document.getElementById('mnivo2').style.display="block";
+	}
+	else {
+		x.innerHTML="+";
+		document.getElementById('mnivo2').style.display="none";
+	}
+}
+
+function master1(){
+	var x=document.getElementById('m1plus');
+	if (x.innerHTML=="+"){
+		x.innerHTML="-";
+		document.getElementById('mpnivo3').style.display="block";
+	}
+	else {
+		x.innerHTML="+";
+		document.getElementById('mpnivo3').style.display="none";
+	}
+}
+
+function master2(){
+	var x=document.getElementById('m2plus');
+	if (x.innerHTML=="+"){
+		x.innerHTML="-";
+		document.getElementById('mdnivo3').style.display="block";
+	}
+	else {
+		x.innerHTML="+";
+		document.getElementById('mdnivo3').style.display="none";
+	}
+}
